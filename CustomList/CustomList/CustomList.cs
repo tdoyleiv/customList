8ae5412aaf88801171c632daef903462b4437d44
Tom Doyle <tfdoyleiv@gmail.com>
2017-12-06 08:30:22 -0600

    Created method signatures, created preliminary get/set & add tests

---
+++ CustomList/CustomList/CustomList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T> where T : IComparer<T>, IEqualityComparer<T>
+    {
+        private int count;
+        private T[] array = new T[] { };
+        public T this[int i] => array[i];
+        public void AddElement(object element)
+        {
+
+        }
+        public void ClearElements()
+        {
+
+        }
+        public void ContainsELement(object element)
+        {
+
+        }
+        public void IndexOfElement(object element)
+        {
+
+        }
+        public void InsertElement(Int32 index, object element)
+        {
+
+        }
+        public void RemoveElementAtIndex(Int32 index)
+        {
+
+        }
+        public void CompareElementTo(object otherElement, IComparer<T> comparableElement)
+        {
+
+        }
+        public void ElementEquals(object otherElement, IEqualityComparer<T> comparableElement)
+        {
+
+        }
+        public void GetElementHashCode(IEqualityComparer<T> comparableElement)
+        {
+
+        }
+    }
+}
---
+++ CustomList/CustomListTesting/CustomListTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTesting
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void get_returnEnteredValue_returnValue()
+        {
+            //Arrange
+            Indexer<TestCategoryAttribute> indexer = new Indexer<TestCategoryAttribute>();
+            int i = 0;
+            //Act
+            TestCategoryAttribute testValue = indexer[i];
+            //Assert
+            Assert.AreEqual(testValue, indexer[i]);
+        }
+
+        [TestMethod]
+        public void get_returnEnteredValue_returnValueEX()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int testValue = 1;
+            //Act
+            testList.Add(1);
+            //Assert
+            Assert.AreEqual(testValue, testList[0]);
+        }
+
+        [TestMethod]
+        // additional attribute for "expect exception thrown"
+        public void get_returnEnteredValue_returnValueEX2()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int testValue = 1;
+            //Act
+            testList.Add(1);
+            //Assert
+            Assert.AreEqual(testValue, testList[10]);
+        }
+    }
+}
