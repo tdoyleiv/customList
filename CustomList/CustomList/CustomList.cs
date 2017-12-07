7bb3ccf96688e0518d3a6c30be7df8ec85af1b10
Tom Doyle <tfdoyleiv@gmail.com>
2017-12-06 21:57:56 -0600

    Created .Add tests, .Add method/helpers

--- CustomList/CustomList/CustomList.cs
+++ CustomList/CustomList/CustomList.cs
@@ -8,32 +8,78 @@ namespace CustomList
 {
     public class CustomList<T>
     {
-        //private int count;
-        private T[] array = new T[] { };
-        public T this[int i] => array[i];
-        public void AddElement(object element)
+        private int count;
+        private int capacity;
+        private T[] array;
+        public T this[int i]
+        {
+            get
+            {
+                return array[i];
+            }
+            set
+            {
+                array[i] = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+            }
+        }
+        public CustomList()
+        {
+            count = 0;
+            capacity = 5;
+            array = new T[capacity];
+        }
+        public void Add(T element)
+        {
+            if (Count < capacity)
+            {
+                array[Count] = element;
+                IncrementCount(Count);
+            }
+            else if (Count >= capacity)
+            {
+                DoubleCapacity(capacity);
+                array[Count] = element;
+                IncrementCount(Count);
+            }
+        }
+        public void Remove(T element)
         {
 
         }
-        public void RemoveElement(object element)
+        public void Contains(T element)
         {
 
         }
-        public void ContainsELement(object element)
+        public void IndexOf(T element)
         {
 
         }
-        public void IndexOfElement(object element)
+        public void Insert(int index, T element)
         {
 
         }
-        public void InsertElement(Int32 index, object element)
+        public void RemoveAt(int index)
         {
 
         }
-        public void RemoveElementAtIndex(Int32 index)
+        public int IncrementCount(int count)
         {
-
+            return Count++;
+        }
+        public int DoubleCapacity(int capacity)
+        {
+           return capacity * 2;
         }
     }
 }
--- CustomList/CustomListTesting/CustomListTest.cs
+++ CustomList/CustomListTesting/CustomListTest.cs
@@ -8,50 +8,112 @@ namespace CustomListTesting
     public class CustomListTest
     {
         [TestMethod]
-        public void getSet_returnEnteredValue_returnValue()
+        public void Indexer_GetValue_ReturnGetValue()
         {
             //Arrange
-            CustomList<TestCategoryAttribute> testList = new CustomList<TestCategoryAttribute>();
+            CustomList<string> testList = new CustomList<string>();
             int i = 0;
             //Act
-            TestCategoryAttribute testValue = testList[i];
+            testList.Add("test");
             //Assert
-            Assert.AreEqual(testValue, testList[i]);
+            Assert.AreEqual("test", testList[i]);
         }
-
+        public void Indexer_SetValue_ReturnSetValue()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+            //Act
+            testList.Add(5);
+            testList[0] = 10;
+            int expected = 10;
+            //Assert
+            Assert.AreEqual(expected, testList[0]);
+        }
         [TestMethod]
-        public void add_addInt_returnValueEX()
+        public void Add_AddInt_ReturnValueEX()
         {
             //Arrange
             CustomList<int> testList = new CustomList<int>();
             int testValue = 1;
             //Act
-            testList.AddElement(testValue);
+            testList.Add(testValue);
             //Assert
             Assert.AreEqual(testValue, testList[0]);
         }
-
         [TestMethod]
-        [ExpectedException(typeof(IndexOutOfRangeException))]
-        public void add_addStringIndex_IndexExceptionExpected()
+        public void Add_AddString_ReturnValueEX2()
         {
             //Arrange
             CustomList<string> testList = new CustomList<string>();
             string testValue = "test";
             //Act
-            testList.AddElement(testValue);
+            testList.Add(testValue);
             //Assert
-            Assert.AreEqual(testValue, testList[10]);
+            Assert.AreEqual(testValue, testList[0]);
         }
-        public void add_addBool_returnValueEX3()
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Add_AddString_IndexExceptionExpected()
+        {
+            //Arrange
+            CustomList<string> testList = new CustomList<string>();
+            //Act
+            string result = testList[10];
+            //Assert
+        }
+        [TestMethod]
+        public void Add_AddBool_IncrementCount()
         {
             //Arrange
             CustomList<bool> testList = new CustomList<bool>();
             bool testValue = false;
             //Act
-            testList.AddElement(testValue);
+            testList.Add(testValue);
             //Assert
-            Assert.AreEqual(testValue, testList[0]);
+            Assert.AreEqual(1, testList.Count);
         }
+        [TestMethod]
+        public void Add_AddDoubleOverCapacity_ExpectedCount()
+        {
+            //Arrange
+            CustomList<double> testList = new CustomList<double>();
+            double testValueOne = 1;
+            double testValueTwo = 2;
+            double testValueThree = 3;
+            double testValueFour = 4;
+            double testValueFive = 5;
+            double testValueSix = 6;
+            //Act
+            testList.Add(testValueOne);
+            testList.Add(testValueTwo);
+            testList.Add(testValueThree);
+            testList.Add(testValueFour);
+            testList.Add(testValueFive);
+            testList.Add(testValueSix);
+            //Assert
+            Assert.AreEqual(6, testList.Count);
+        }
+        [TestMethod]
+        public void Add_AddUIntOverCapacity_ExpectedList()
+        {
+            CustomList<uint> testList = new CustomList<uint>();
+            //Arrange
+            uint testValueOne = 1;
+            uint testValueTwo = 2;
+            uint testValueThree = 3;
+            uint testValueFour = 4;
+            uint testValueFive = 5;
+            uint testValueSix = 6;
+            //Act
+            testList.Add(testValueOne);
+            testList.Add(testValueTwo);
+            testList.Add(testValueThree);
+            testList.Add(testValueFour);
+            testList.Add(testValueFive);
+            testList.Add(testValueSix);
+            //Assert
+            //Foreach...get list
+        }
+
     }
 }
